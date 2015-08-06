c9b962a5753fb258ffa5ceaaaf68799e8e16834e
Shidhin Sarath <shidhin@whatznear.com>
2015-08-06 17:00:41 +0530

    simplify product.variant_images

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,4 +1,4 @@
 Spree::Variant.class_eval do
   has_many :variant_images, -> { order(:position) }, class_name: '::Spree::VariantImage'
-  has_many :images, through: :variant_images, source: :image
+  has_many :images, -> { order(:position) }, through: :variant_images, source: :image # for @product.images.first to return correctly
 end
